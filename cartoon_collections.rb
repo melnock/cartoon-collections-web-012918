bc982941d7be18de75d725b57676d3ac26df23a7
Melissa Nock <melnock@gmail.com>
2018-01-17 16:41:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(arr)
   arr.map { |x| x.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  
 end
 
 def find_the_cheese# code an argument here
