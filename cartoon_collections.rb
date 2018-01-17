19a0cfaf9d32b5553d55801404c4fbd56d049526
Melissa Nock <melnock@gmail.com>
2018-01-17 16:40:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(arr)
 end
 
 def summon_captain_planet(arr)
-  arr.map { |x| return x.capitalize + "!" }
+  arr.map { |x| x.capitalize + "!" }
 end
 
 def long_planeteer_calls# code an argument here
